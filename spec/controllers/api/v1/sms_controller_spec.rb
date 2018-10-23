e1cd89718af156c8a6120e21138d42e4c8f784e6
Denn Orina <anyoksdenn@gmail.com>
2018-10-23 12:04:54 +0000

    added test files

--- spec/controllers/api/v1/sms_controller_spec.rb
+++ spec/controllers/api/v1/sms_controller_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::SmsController, type: :controller do
 
+
+	describe 'Make booking' do
+
+		it 'returns a json for sms' do
+
+			get :create, format: :json
+
+		    expect(response).to be_success
+		end
+
+	end
+
 end
--- spec/models/sms_spec.rb
+++ spec/models/sms_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Sm, type: :model do
+RSpec.describe Sms, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
