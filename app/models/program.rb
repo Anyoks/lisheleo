bbb4bf9f5fb883a9ff5d8a78a47e2e46a33a9ab1
Denn Orina <anyoksdenn@gmail.com>
2018-11-16 14:02:46 +0000

    added helper methods

--- app/models/program.rb
+++ app/models/program.rb
@@ -21,4 +21,59 @@ class Program < ApplicationRecord
 		program.sms_description.nil? ?   program.sms_description : program.sms_description = program.sms_description.downcase  }
 
 	has_many :available_times
+	has_many :bookings
+	has_many :sms
+
+
+	def duration_in_seconds
+		self.duration * 60
+	end
+
+# 	max bookings per day
+	def max_bookings
+		
+	end
+
+	def sessions
+
+		days = []
+		times = []
+
+		self.available_times.each do |time|
+			days << time.day
+			times << time.time
+		end
+		hash = Hash[*days.zip(times).flatten]
+		return hash			
+	end
+
+	def sessions_for_day day
+		time = self.available_times.where(day: day).first
+
+		if time.present?
+			return time.time
+		else
+			return false
+		end
+	end
+
+	def days
+		days = []
+
+		self.available_times.each do |time|
+			days << time.day
+		end
+
+		return days
+	end
+
+	def times
+		times = []
+
+		self.available_times.each do |time|
+			times << time.time
+		end
+
+		return times
+	end
 end
