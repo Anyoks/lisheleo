ce2ba1e4ce5834d393dc658cd595ddba1a661332
Denn Orina <anyoksdenn@gmail.com>
2018-11-16 13:59:17 +0000

    added helper methods

--- app/models/available_time.rb
+++ app/models/available_time.rb
@@ -13,4 +13,24 @@
 
 class AvailableTime < ApplicationRecord
 	belongs_to :program
+
+
+
+	def time
+		time = self.start_time.strftime("%I:%M %p")
+		end_time = self.end_time.strftime("%I:%M %p")
+
+		return time + ' - ' + end_time
+	end
+
+	# formated time
+	def fstart_time
+		self.start_time.strftime("%I:%M %p")
+	end
+
+	def fend_time
+		self.start_time.strftime("%I:%M %p")
+	end
+
+
 end
