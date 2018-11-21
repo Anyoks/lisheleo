433e3e850169a3a2acecc0cd9519ff1ac398eb1c
Denn Orina <anyoksdenn@gmail.com>
2018-11-21 18:44:52 +0000

    added methods to find available time slots and validate uniqness of time and program

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -16,6 +16,8 @@
 
 class Booking < ApplicationRecord
 
+	validates :time, uniqueness: { scope: [:program_id ]} #, :another_medium] }
+
 	belongs_to :sms
 	belongs_to :client
 	belongs_to :program
@@ -60,31 +62,194 @@ class Booking < ApplicationRecord
 			# convert time to have same date as available time then check
 			# if time >= avalable.start_time  && time + duration < available.endtime
 			
+			
 			# returns "02:00 PM - 04:00 PM" 
 			program_time = self.program.sessions_for_day self.day 
 
 			program_start_time = program_time.split('-')[0]
 			program_end_time = program_time.split('-')[1]
 
-			if self.time >= Time.zone.parse(program_start_time) && self.end_time <= Time.zone.parse(program_end_time)
-				# # check if a booking already exists
+			# check if the user's proposed booking time is within the allowed booking hours
+			if self.time >= Time.zone.parse(program_start_time) || self.end_time < Time.zone.parse(program_end_time)
+				# # check if a booking already exists for that particular time
 				# 
 				# check if there's a booking for this program on this date
-				bookings = Booking.where(date: self.date, program_id: self.id)
+				bookings = Booking.where(date: self.date, program_id: self.program.id)
 
 				# check each bookings time
 				if bookings.present?
-					# how do I search the start times for this collection to see compare with the booking time
-					# 
 					
+					# check if max bookings for this day have been reached
+					if bookings.count >= self.program.max_bookings(self.day)
+
+						# I'm thinking I should query available slots and present the user with proposed time
+						# slots for booking as opposed to leave them guessing till they get it right.
+						# Maybe return a 2 day list of available slot times, e. Monday 12 2pm, 3pm 4pm, Tue 13 8am, 9am 
+						# wed, 3pm etc
+						
+						# return false, "This day is fully booked"
+						return available_slots_this_week
+					else
+						# check if the users' time slot is already taken
+						#check the start times for this session and tell give the user a choice if thier's
+						#don't fall in that range.
+						
+						ending = self.f_end_time
+
+						# find starting times then select for this day
+						# 
+						# search Db for which one exists and suggest the other one.
+						checking = self.program.get_starting_times_for_sessions # e.g {"Tuesday"=>["02:00 PM ", "02:50 PM"], "Thursday"=>["08:30 AM "]} 
+						today    = checking[self.day] # e.g ["02:00 PM ", "02:50 PM"] 
+
+						counter = 0
+
+						# loop do
+							
+						# end
+
+						while counter <= today.size
+							
+							if bookings.where(end_time: Time.zone.parse(today[counter])).present?
+								# this time slot has been taken.
+								# return suggested time slots for today
+								puts "Here today  #{today}  counter #{counter}"
+								return false , today[counter+1]# "This slot is taken but there's one available today"
+								break
+							else
+								# now we can pick a time and book a slot or suggest a time slot to the user
+								puts "Here today1  #{today}  counter1 #{counter}"
+								return true, today[counter] #{}"This time slot is present"
+								break
+							end
+
+							counter +=1
+						end
+						# will this where clause bring times from the past? 
+												
+					end
+				else
+					return true, "there are no booking today You can book successfully"
 				end
 
-				return true
+				# return true
 			else
 				return false," time is out of range, either too early or too late."
 			end
 		else
+
 			return false, "Picked the wrong day"
+		end
+	end
+
+	def available_slots_this_week
+		days 	 = self.program.days #.include?(self.day)
+		bookings = Booking.where(date: self.date, program_id: self.id)
+		program  = self.program
+
+		# starting date is today's date
+		
+
+		total_bookings_possible = program.week_max_bookings #possible bookings for this program
+
+
+		if advance_date.class.name != "Array"
+
+			this_week_bookings = program.bookings.where(date: Date.today..advance_date )  #put a date range for this week and next week
+			
+			if this_week_bookings.count == total_bookings_possible
+				
+				# "This week is fully booked"
+				# return next weeks available time slots
+				return false, self.program.get_only_next_weeks_dates
+			else
+				# return the available slot start_times, days and dates. i,e Monday 12th 9am
+				return true ,self.program.get_only_future_days
+			end
+
+			# 
+			# next_week_bookings = program.bookings.where(date: Date.today.next_week..Date.today.next_week.advance(days: 4) )
+			
+			# return this_week_bookings.count #, next_week_bookings.count  #put a date range for this week and next week
+		else
+			return false, advance[1]
+		end
+	end
+
+	def available_slots_next_week
+		days 	 = self.program.days #.include?(self.day)
+		bookings = Booking.where(date: self.date, program_id: self.id)
+		program  = self.program
+
+		# starting date is today's date
+		
+
+		total_bookings_possible = program.week_max_bookings #possible bookings for this program
+
+
+		if next_week_advance_date.class.name != "Array"
+
+			next_week_bookings = program.bookings.where(date: Date.today.next_week..next_week_advance_date )  #put a date range for this week and next week
+			
+			if next_week_bookings.count == total_bookings_possible
+				
+				# return 0, "This week is fully booked"
+				# return next weeks available time slots
+				return false, self.program.get_only_next_weeks_dates # get slots for the other week
+			else
+				# return the available slot start_times, days and dates. i,e Monday 12th 9am
+				return true, self.program.get_only_next_weeks_dates
+			end
+
+			# 
+			# next_week_bookings = program.bookings.where(date: Date.today.next_week..Date.today.next_week.advance(days: 4) )
+			
+			# return next_week_bookings.count #, next_week_bookings.count  #put a date range for this week and next week
+		else
+			return false, advance[1]
+		end
+	end
+
+	def advance_date
+		#  0 - Sunday, 5 - Friday
+		today = Date.today.wday
+		if today == 0 # if today is Sun
+			return false, "no bookings on sunday"
+		elsif today == 1 # Monday
+			return Date.today.advance( days: 4)
+		elsif today == 2 # Tue
+			return Date.today.advance( days: 3)
+		elsif today == 3 # Wed
+			return  Date.today.advance( days: 2)
+		elsif today == 4 # Thur
+			return Date.today.advance( days: 1)
+		elsif today == 5 # Fi
+			return Date.today.advance( days: 0)
+		elsif today == 6 # Thur
+			return false, "No bookings on Sabbath"
+		else
+			return false, "Out of range" # out of range
+		end
+	end
+
+	def next_week_advance_date
+		#  0 - Monday, 4 - Friday
+		monday = Date.today.next_week.wday
+		
+		if monday == 1 # Monday
+			return Date.today.next_week.advance( days: 4)
+		elsif monday == 2 # Tue
+			return Date.today.next_week.advance( days: 3)
+		elsif monday == 3 # Wed
+			return  Date.today.next_week.advance( days: 2)
+		elsif monday == 4 # Thur
+			return Date.today.next_week.advance( days: 1)
+		elsif monday == 5 # Fi
+			return Date.today.next_week.advance( days: 0)
+		elsif monday == 6 # Thur
+			return false, "No bookings on Sabbath"
+		else
+			return false, "Out of range"# out of range
 		end
 	end
 
@@ -151,5 +316,4 @@ class Booking < ApplicationRecord
 		
 		return hrs,min
 	end
-
 end
