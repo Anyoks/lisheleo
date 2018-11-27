37a7c7793c816673f2a47540176f687e3ffe7ca2
Denn Orina <anyoksdenn@gmail.com>
2018-11-27 20:38:21 +0000

    uncommented this for seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,95 +14,95 @@
 
 require 'csv'
 
-# programs_csv_text = File.read(Rails.root.join('lib', 'seeds', 'programs.csv'))
-# programs_csv = CSV.parse(programs_csv_text, :headers => true, :encoding => 'ISO-8859-1')
+programs_csv_text = File.read(Rails.root.join('lib', 'seeds', 'programs.csv'))
+programs_csv = CSV.parse(programs_csv_text, :headers => true, :encoding => 'ISO-8859-1')
 
-# programs_csv.each do |row|
-# 	# puts row.to_hash
+programs_csv.each do |row|
+	# puts row.to_hash
 
-# 	name 			= row['Program']
-# 	description 	= row['Description']
-# 	fixed 			= row['Fixed']
-# 	confirmation 	= row['confirmation']
-# 	parallel 		= row['Parallel']
-# 	sms_description	= row['Short sms Desc']
-
-
-# 	# check if the program exists or create it.
-# 	Program.where(name: "#{name}").first_or_create do |prog|
-# 		prog.description 		= description
-# 		prog.fixed 		 		= fixed
-# 		prog.confirmation		= confirmation
-# 		prog.parallel 			= parallel
-# 		prog.sms_description	= sms_description
-# 		prog.save
-
-# 		puts "Program => #{name}, === SAVED!"
-# 	end
-# 	# puts "\n"
-# 	# puts "Program => #{name}, Desc => #{description}, Fixed => #{fixed}, confirmation => #{confirmation}, Parallel => #{parallel},  Sms => #{sms_description}"
-# 	# puts "\n"
-# end
+	name 			= row['Program']
+	description 	= row['Description']
+	fixed 			= row['Fixed']
+	confirmation 	= row['confirmation']
+	parallel 		= row['Parallel']
+	sms_description	= row['Short sms Desc']
 
 
-# time_csv_text = File.read(Rails.root.join('lib', 'seeds', 'available_time.csv'))
-# time_csv      = CSV.parse(time_csv_text, :headers => true, :encoding => 'ISO-8859-1')
+	# check if the program exists or create it.
+	Program.where(name: "#{name}").first_or_create do |prog|
+		prog.description 		= description
+		prog.fixed 		 		= fixed
+		prog.confirmation		= confirmation
+		prog.parallel 			= parallel
+		prog.sms_description	= sms_description
+		prog.save
+
+		puts "Program => #{name}, === SAVED!"
+	end
+	# puts "\n"
+	# puts "Program => #{name}, Desc => #{description}, Fixed => #{fixed}, confirmation => #{confirmation}, Parallel => #{parallel},  Sms => #{sms_description}"
+	# puts "\n"
+end
 
 
-# time_csv.each do |row|
-# 	# puts row.to_hash
+time_csv_text = File.read(Rails.root.join('lib', 'seeds', 'available_time.csv'))
+time_csv      = CSV.parse(time_csv_text, :headers => true, :encoding => 'ISO-8859-1')
 
-# 	prog_name 			= row['Program'].downcase
-# 	day			 		= row['Day'].strip
-# 	start_time_raw 		= row['Start time']
-# 	end_time_raw	 	= row['End time']
 
-# 	start_hrs			= start_time_raw.split('')[0..1].join
-# 	start_mins 			= start_time_raw.split('')[2..3].join
-# 	end_hrs				= end_time_raw.split('')[0..1].join
-# 	end_mins 			= end_time_raw.split('')[2..3].join
+time_csv.each do |row|
+	# puts row.to_hash
 
-# 	# month				= 01
-# 	# year				= 2018
-# 	# day 				= 1
-# 	# secs 				= 00
+	prog_name 			= row['Program'].downcase
+	day			 		= row['Day'].strip
+	start_time_raw 		= row['Start time']
+	end_time_raw	 	= row['End time']
 
-# 	start_time 			= Time.zone.parse("#{start_hrs}:#{start_mins}")
-# 	end_time 			= Time.zone.parse("#{end_hrs}:#{end_mins}")
+	start_hrs			= start_time_raw.split('')[0..1].join
+	start_mins 			= start_time_raw.split('')[2..3].join
+	end_hrs				= end_time_raw.split('')[0..1].join
+	end_mins 			= end_time_raw.split('')[2..3].join
+
+	# month				= 01
+	# year				= 2018
+	# day 				= 1
+	# secs 				= 00
+
+	start_time 			= Time.zone.parse("#{start_hrs}:#{start_mins}")
+	end_time 			= Time.zone.parse("#{end_hrs}:#{end_mins}")
 
 	
-# 	# puts "Time #{start_time}  end #{end_time}"
-# 	puts "found---> #{start_time.min}"
+	# puts "Time #{start_time}  end #{end_time}"
+	puts "found---> #{start_time.min}"
 
 
-# 	# check if the program exists 
-# 	program = Program.where(name: "#{prog_name}").first
+	# check if the program exists 
+	program = Program.where(name: "#{prog_name}").first
 
-# 	search_time_start = start_time.hour.to_s + start_time.min.to_s
-# 	search_time_end	  = end_time.hour.to_s + end_time.min.to_s
+	search_time_start = start_time.hour.to_s + start_time.min.to_s
+	search_time_end	  = end_time.hour.to_s + end_time.min.to_s
 
-# 	if program
-# 		puts "found #{start_hrs.to_s}#{start_mins.to_s}"
-# 		puts "found--- #{start_time.hour.to_s}"
-# 		puts "found--- #{start_time.min}"
+	if program
+		puts "found #{start_hrs.to_s}#{start_mins.to_s}"
+		puts "found--- #{start_time.hour.to_s}"
+		puts "found--- #{start_time.min}"
 
-# 		puts "\n"
-# 	 	program.available_times.where( start_time: start_time, end_time: end_time).first_or_create do |time|
+		puts "\n"
+	 	program.available_times.where( start_time: start_time, end_time: end_time).first_or_create do |time|
 			
-# 			time.day 		= day
-# 			time.start_time = start_time # need to format time
-# 			time.end_time   = end_time
-# 			time.save
+			time.day 		= day
+			time.start_time = start_time # need to format time
+			time.end_time   = end_time
+			time.save
 
-# 			puts "Time #{start_time}  end #{end_time}"
-# 		end
-# 	else
-# 		puts "#{prog_name} Not found"
-# 	end
-# 	# puts "\n"
-# 	# puts "Program => #{name}, Desc => #{description}, Fixed => #{fixed}, confirmation => #{confirmation}, Parallel => #{parallel},  Sms => #{sms_description}"
-# 	# puts "\n"
-# end
+			puts "Time #{start_time}  end #{end_time}"
+		end
+	else
+		puts "#{prog_name} Not found"
+	end
+	# puts "\n"
+	# puts "Program => #{name}, Desc => #{description}, Fixed => #{fixed}, confirmation => #{confirmation}, Parallel => #{parallel},  Sms => #{sms_description}"
+	# puts "\n"
+end
 
 names = ['personalized wellness','therapeutic massage','keep fit','cancer support group']
 codes = ['B1','B2','B3','B4']
