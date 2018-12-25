fafdb5dff91158f073f78266ad98b39208e10019
Denn Orina <anyoksdenn@gmail.com>
2018-12-25 21:19:28 +0300

    dynamically load programs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,21 +20,92 @@
 //= require fullcalendar
 //= require jquery-ui/widgets/draggable
 //= require jquery-ui/widgets/droppable
+//= require jquery-ui/widgets/datepicker
 //= require app
 //= require_tree .
 
 
-var initialize_calendar;
+// var initialize_calendar;
+// var get_programs;
 
-initialize_calendar = $(function () {
 
+
+// get_programs =  $(function () {
+//     $.ajax({
+//         url: '/programs.json',                              
+//         type: 'GET',
+//         dataType: 'json',
+//         complete: function (jqXHR, textStatus) {
+//                 // callback
+//         },
+//         success: function (data, textStatus, jqXHR) {
+//         //   alert(data);
+//           // from here on you could also get the product id 
+//           // or whatever you need
+//           var x = 6;
+//          $.each(data, function(k)  { 
+             
+//             //  console.log(data[k].name); // name of products
+//              $(function(){
+//                 console.log("adding div " + x + " " + data[k].name );
+//                 $('#my-draggable').append("<div class=\"external-event " + x + " ui-draggable ui-draggable-handle\" style= \"background-color: rgb(114, 175, 210); border-color: rgb(114, 175, 210); color: rgb(255, 255, 255); position: relative; z-index: 10\">" + data[k].name +"</div");
+//                 $('.external-event.' + x).data('event', { title: data[k].name, duration: '00:50', color: '#257e4a' });
+//              });
+             
+//              x = x+1;
+//             });
+
+//             return true;
+//         },
+//         error: function (jqXHR, textStatus, errorThrown) {
+//             // error callback
+//             return false;
+//         }
+//     });
+// });
+
+function  GetPrograms(callback){
+    $.ajax({
+                url: '/programs.json',                              
+                type: 'GET',
+                dataType: 'json',
+                complete: function (jqXHR, textStatus) {
+                        // callback
+                },
+                success: function (data, textStatus, jqXHR) {
+                //   alert(data);
+                  // from here on you could also get the product id 
+                  // or whatever you need
+                  var x = 6;
+                 $.each(data, function(k)  { 
+                     
+                    //  console.log(data[k].name); // name of products
+                     $(function(){
+                        console.log("adding div " + x + " " + data[k].name );
+                        $('#my-draggable').append("<div class=\"external-event " + x + " ui-draggable ui-draggable-handle\" style= \"background-color: rgb(114, 175, 210); border-color: rgb(114, 175, 210); color: rgb(255, 255, 255); position: relative; z-index: 10\">" + data[k].name +"</div");
+                        $('.external-event.' + x).data('event', { title: data[k].name, duration: '00:50', color: '#257e4a' });
+                     });
+                     
+                     x = x+1;
+                    });
+        
+                   callback(); // call the calendar funct
+                   return;
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    // error callback
+                    return false;
+                }
+            });
+}
+
+function initCal(){
     $('.external-event').draggable(
         {
             revert: true,      // immediately snap back to original position
             revertDuration: 0,  //
         }
     );
-
 
     $('.external-event.1').data('event', { title: 'Personalized wellness', duration: '00:50', color: '#257e4a' });
     $('.external-event.2').data('event', { title: 'Therapeutic Massage', duration: '00:50', color: '#f39c12' });
@@ -59,6 +130,11 @@ initialize_calendar = $(function () {
             events: '/dashboard.json',
             textColor: 'white',
             weekends: false,
+            // select: function(start, end){
+            //     $.getScrip('/bookings/new',function(){
+
+            //     });
+            // },
             drop: function (date,jsEvent, ui, resourceId) {
                 alert("Dropped on " + date.format());
 
@@ -69,9 +145,52 @@ initialize_calendar = $(function () {
                 console.log("ttt" + duration);
                 console.log("rrr" + title);
                 var timeDuration = moment.duration(duration); // convert string into duration
-                
                 var end = date.clone().add(timeDuration); // on drop we only have date given to us add that to the start
-                console.log('end is ' + end.format());
+
+                // get the time to auto pupulate start time field
+                var hour       = date.format('HH');
+                var mins       = date.format('mm');
+                var sec        = date.format('ss');
+                var year       = date.format('YYYY');
+                var month      = date.format('MM');
+                var day        = date.format('D');
+
+                // get the time to auto pupulate end time field
+                var end_hour   = end.format('HH');
+                var end_mins   = end.format('mm');
+                var end_sec    = end.format('ss');
+                var end_year   = end.format('YYYY');
+                var end_month  = end.format('MM');
+                var end_day    = end.format('D');
+        
+
+                
+                console.log('QQQQQQ' + ' ' + year + ' ' +  month + ' ' + day + ' ' + hour + ' ' + mins + ' ' + sec);
+
+                console.log('EEEEE' + ' ' + end_year + ' ' +  end_month + ' ' + end_day + ' ' + end_hour + ' ' + end_mins + ' ' + end_sec);
+                $.getScript('/bookings/new',function(){
+
+                    // Populate the data fields based on where the event was dropped.
+                    $('#booking_description').val(title);
+                    $('#booking_time_1i').val( year); // YEAR
+                    $('#booking_time_2i').val(month); // month
+                    $('#booking_time_3i').val(day); // day
+                    $('#booking_time_4i').val(hour); // hour
+                    $('#booking_time_5i').val(mins); // minutes
+
+                    //end time
+                    $('#booking_end_time_1i').val(end_year); // YEAR
+                    $('#booking_end_time_2i').val(end_month); // month
+                    $('#booking_end_time_3i').val(end_day); // day
+                    $('#booking_end_time_4i').val(end_hour); // hour
+                    $('#booking_end_time_5i').val(end_mins); // minutes
+                    
+    
+                });
+
+                // var test = get_programs;
+
+                console.log('GGGGGGGGGGGG' + test);
             },
             eventReceive: function(event){
                 console.log("Event Drop call back");
@@ -82,15 +201,9 @@ initialize_calendar = $(function () {
             }
         }
     );
+}
 
-
-
-
-})
-
-$(document).on('turbolinks:load', initialize_calendar);
-
-// $(function () {
+// initialize_calendar = $(function () {
 
 //     $('.external-event').draggable(
 //         {
@@ -98,7 +211,6 @@ $(document).on('turbolinks:load', initialize_calendar);
 //             revertDuration: 0,  //
 //         }
 //     );
-
 
 //     $('.external-event.1').data('event', { title: 'Personalized wellness', duration: '00:50', color: '#257e4a' });
 //     $('.external-event.2').data('event', { title: 'Therapeutic Massage', duration: '00:50', color: '#f39c12' });
@@ -115,12 +227,19 @@ $(document).on('turbolinks:load', initialize_calendar);
 //                 center: 'title',
 //                 right: 'month,agendaWeek,agendaDay'
 //             },
+//             selectable: true,
+//             editable: true,
 //             defaultView: 'agendaWeek', // changing view to agenda view    
 //             minTime: '06:00:00',
 //             maxTime: '19:00:00',
 //             events: '/dashboard.json',
 //             textColor: 'white',
 //             weekends: false,
+//             // select: function(start, end){
+//             //     $.getScrip('/bookings/new',function(){
+
+//             //     });
+//             // },
 //             drop: function (date,jsEvent, ui, resourceId) {
 //                 alert("Dropped on " + date.format());
 
@@ -131,9 +250,52 @@ $(document).on('turbolinks:load', initialize_calendar);
 //                 console.log("ttt" + duration);
 //                 console.log("rrr" + title);
 //                 var timeDuration = moment.duration(duration); // convert string into duration
-                
 //                 var end = date.clone().add(timeDuration); // on drop we only have date given to us add that to the start
-//                 console.log('end is ' + end.format());
+
+//                 // get the time to auto pupulate start time field
+//                 var hour       = date.format('HH');
+//                 var mins       = date.format('mm');
+//                 var sec        = date.format('ss');
+//                 var year       = date.format('YYYY');
+//                 var month      = date.format('MM');
+//                 var day        = date.format('D');
+
+//                 // get the time to auto pupulate end time field
+//                 var end_hour   = end.format('HH');
+//                 var end_mins   = end.format('mm');
+//                 var end_sec    = end.format('ss');
+//                 var end_year   = end.format('YYYY');
+//                 var end_month  = end.format('MM');
+//                 var end_day    = end.format('D');
+        
+
+                
+//                 console.log('QQQQQQ' + ' ' + year + ' ' +  month + ' ' + day + ' ' + hour + ' ' + mins + ' ' + sec);
+
+//                 console.log('EEEEE' + ' ' + end_year + ' ' +  end_month + ' ' + end_day + ' ' + end_hour + ' ' + end_mins + ' ' + end_sec);
+//                 $.getScript('/bookings/new',function(){
+
+//                     // Populate the data fields based on where the event was dropped.
+//                     $('#booking_description').val(title);
+//                     $('#booking_time_1i').val( year); // YEAR
+//                     $('#booking_time_2i').val(month); // month
+//                     $('#booking_time_3i').val(day); // day
+//                     $('#booking_time_4i').val(hour); // hour
+//                     $('#booking_time_5i').val(mins); // minutes
+
+//                     //end time
+//                     $('#booking_end_time_1i').val(end_year); // YEAR
+//                     $('#booking_end_time_2i').val(end_month); // month
+//                     $('#booking_end_time_3i').val(end_day); // day
+//                     $('#booking_end_time_4i').val(end_hour); // hour
+//                     $('#booking_end_time_5i').val(end_mins); // minutes
+                    
+    
+//                 });
+
+//                 var test = get_programs;
+
+//                 console.log('GGGGGGGGGGGG' + test);
 //             },
 //             eventReceive: function(event){
 //                 console.log("Event Drop call back");
@@ -149,6 +311,6 @@ $(document).on('turbolinks:load', initialize_calendar);
 
 
 // })
-
-
-
+// $(document).on('turbolinks:load',get_programs );
+// $(document).on('turbolinks:load',initialize_calendar );
+$(document).on('turbolinks:load',GetPrograms(initCal) );
