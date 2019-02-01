2c19a2e41fbaae50853f5bc4820ee0db6cbc427b
Denn Orina <anyoksdenn@gmail.com>
2019-02-01 15:35:37 +0000

    updated seeds to upload availble times

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@
 # 
 
 
-# require 'csv'
+require 'csv'
 
 # programs_csv_text = File.read(Rails.root.join('lib', 'seeds', 'programs.csv'))
 # programs_csv = CSV.parse(programs_csv_text, :headers => true, :encoding => 'ISO-8859-1')
@@ -45,64 +45,75 @@
 # end
 
 
-# time_csv_text = File.read(Rails.root.join('lib', 'seeds', 'available_time.csv'))
-# time_csv      = CSV.parse(time_csv_text, :headers => true, :encoding => 'ISO-8859-1')
+time_csv_text = File.read(Rails.root.join('lib', 'seeds', 'available_time.csv'))
+time_csv      = CSV.parse(time_csv_text, :headers => true, :encoding => 'ISO-8859-1')
 
 
-# time_csv.each do |row|
-# 	# puts row.to_hash
+time_csv.each do |row|
+	# puts row.to_hash
 
-# 	prog_name 			= row['Program'].downcase
-# 	day			 		= row['Day'].strip
-# 	start_time_raw 		= row['Start time']
-# 	end_time_raw	 	= row['End time']
+	prog_name 			= row['Program'].downcase
+	day			 		= row['Day'].strip
+	start_time_raw 		= row['Start time']
+	end_time_raw	 	= row['End time']
 
-# 	start_hrs			= start_time_raw.split('')[0..1].join
-# 	start_mins 			= start_time_raw.split('')[2..3].join
-# 	end_hrs				= end_time_raw.split('')[0..1].join
-# 	end_mins 			= end_time_raw.split('')[2..3].join
+	start_hrs			= start_time_raw.split('')[0..1].join
+	start_mins 			= start_time_raw.split('')[2..3].join
+	end_hrs				= end_time_raw.split('')[0..1].join
+	end_mins 			= end_time_raw.split('')[2..3].join
 
-# 	# month				= 01
-# 	# year				= 2018
-# 	# day 				= 1
-# 	# secs 				= 00
+	# month				= 01
+	# year				= 2018
+	# day 				= 1
+	# secs 				= 00
 
-# 	start_time 			= Time.zone.parse("#{start_hrs}:#{start_mins}")
-# 	end_time 			= Time.zone.parse("#{end_hrs}:#{end_mins}")
+	start_time 			= Time.zone.parse("#{start_hrs}:#{start_mins}")
+	end_time 			= Time.zone.parse("#{end_hrs}:#{end_mins}")
 
 	
-# 	# puts "Time #{start_time}  end #{end_time}"
-# 	puts "found---> #{start_time.min}"
+	# puts "Time #{start_time}  end #{end_time}"
+	# puts "found---> #{start_time.min}"
 
 
-# 	# check if the program exists 
-# 	program = Program.where(name: "#{prog_name}").first
+	# check if the program exists 
+	program = Program.where(name: "#{prog_name}").first
 
-# 	search_time_start = start_time.hour.to_s + start_time.min.to_s
-# 	search_time_end	  = end_time.hour.to_s + end_time.min.to_s
+	search_time_start = start_time.hour.to_s + start_time.min.to_s
+	search_time_end	  = end_time.hour.to_s + end_time.min.to_s
 
-# 	if program
-# 		puts "found #{start_hrs.to_s}#{start_mins.to_s}"
-# 		puts "found--- #{start_time.hour.to_s}"
-# 		puts "found--- #{start_time.min}"
+	if program
+		# puts "PROGRAM #{program.name} "
+		# puts "Day:: #{day}"
+		# puts "Start time & end time:: #{start_time} #{end_time}"
+		# # puts "found--- #{start_time.min}"
 
-# 		puts "\n"
-# 	 	program.available_times.where( start_time: start_time, end_time: end_time).first_or_create do |time|
+		puts "\n"
+		if program.available_times.where( day: day ,start_time: start_time, end_time: end_time).present?
+			puts "#{program.name} ->#{day} :: #{start_time} #{end_time} ALREADY PRESENT"
+		else
 			
-# 			time.day 		= day
-# 			time.start_time = start_time # need to format time
-# 			time.end_time   = end_time
-# 			time.save
+			program.available_times.where( day: day ,start_time: start_time, end_time: end_time).first_or_create do |time|
+			
+				time.day 		= day
+				time.start_time = start_time # need to format time
+				time.end_time   = end_time
+				# time.save
+	
+				# puts "Time #{start_time}  end #{end_time}"
+				puts "#{program.name} -> #{day} :: #{start_time} #{end_time} CREATED"
+				
+				puts "\n"
+			end
+		end
 
-# 			puts "Time #{start_time}  end #{end_time}"
-# 		end
-# 	else
-# 		puts "#{prog_name} Not found"
-# 	end
-# 	# puts "\n"
-# 	# puts "Program => #{name}, Desc => #{description}, Fixed => #{fixed}, confirmation => #{confirmation}, Parallel => #{parallel},  Sms => #{sms_description}"
-# 	# puts "\n"
-# end
+	 	
+	else
+		puts "#{prog_name} Not found"
+	end
+	# puts "\n"
+	# puts "Program => #{name}, Desc => #{description}, Fixed => #{fixed}, confirmation => #{confirmation}, Parallel => #{parallel},  Sms => #{sms_description}"
+	# puts "\n"
+end
 
 # names = ['personalized wellness','therapeutic massage','keep fit','cancer support group']
 # codes = ['B1','B2','B3','B4']
@@ -118,6 +129,6 @@
 # 	i += 1
 # end
 
-['create', 'update', 'delete'].each do |type|
-	ActivityType.find_or_create_by({name: type})
-end
+# ['create', 'update', 'delete'].each do |type|
+# 	ActivityType.find_or_create_by({name: type})
+# end
